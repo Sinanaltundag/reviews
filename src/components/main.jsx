00db68114f2521dfe955c7023b3bb0ec8472757f
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-13 22:35:16 +0300

    clear codes publish github

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { ReactComponent as NextIcon } from "../svgs/next.svg";
-import { ReactComponent as PrevIcon } from "../svgs/previous.svg";
 import { ReactComponent as TickIcon } from "../svgs/tick.svg";
 import { Data } from "./data";
 
